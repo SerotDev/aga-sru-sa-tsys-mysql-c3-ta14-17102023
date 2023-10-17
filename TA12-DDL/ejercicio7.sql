007955ab725893198432572ec07efb6f569e7a0d
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-10-17 18:22:14 +0200

    Update ejercicio7.sql

--- TA12-DDL/ejercicio7.sql
+++ TA12-DDL/ejercicio7.sql
@@ -17,3 +17,31 @@ create table Trabajador(
 	foreign key (cod_trabajador_repsonsable) references Trabajador (codigo) on delete cascade on update cascade
 
 );
+
+create table Departamento(
+	codigo int auto_increment,
+	nombre varchar(20),
+    cod_trabajador int,
+    primary key (codigo),
+    foreign key (cod_trabajador) references Trabajador (codigo) on delete cascade on update cascade
+);
+
+create table Contrato(
+	id int auto_increment,
+	fecha_ini date,
+    fecha_fin date,
+    categoria_profesional varchar(20),
+    cod_trabajador int,
+    primary key (id),
+    foreign key (cod_trabajador) references Trabajador (codigo) on delete cascade on update cascade
+);
+
+create table Nomina(
+	id int auto_increment,
+	fecha date,
+    salario double,
+    categoria_profesional varchar(20),
+    id_contrato int,
+    primary key (id),
+    foreign key (id_contrato) references Contrato (id) on delete cascade on update cascade
+);
