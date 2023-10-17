f8b0d5f828308d484a1d58294ba9af27f2c9bab4
sergial273 <97388630+sergial273@users.noreply.github.com>
2023-10-17 18:33:35 +0200

    Update ejercicio8.sql

--- TA12-DDL/ejercicio8.sql
+++ TA12-DDL/ejercicio8.sql
@@ -42,16 +42,29 @@ create table Asiento(
     foreign key (id_avion) references Avion (id) on delete cascade on update cascade
 );
 
-create table Vuelo(
+create table Tarjeta_embarque(
 	id int auto_increment,
-    plazas int,
-    fecha_salida date,
-    fecha_llegada date,
-    id_aeropuerto_llegada int,
-    id_aeropuerto_salida int,
-    id_avion int,
+    num_reserva int,
+    fecha date,
+    id_asiento int,
     primary key (id),
-    foreign key (id_aeropuerto_llegada) references Aeropuerto (id) on delete cascade on update cascade,
-	foreign key (id_aeropuerto_salida) references Aeropuerto (id) on delete cascade on update cascade,
-	foreign key (id_avion) references Avion (id) on delete cascade on update cascade
+    foreign key (id_asiento) references Asiento (id) on delete cascade on update cascade
+);
+
+create table Cliente(
+	id int auto_increment,
+    nombre varchar(20),
+    apellidos varchar(20),
+    direccion varchar(20),
+    telefono varchar(20),
+    tarjeta_credito varchar(20),
+    primary key (id)
+);
+
+create table Reserva(
+	dni_cliente int,
+    id_tarjeta_embarque int,
+    primary key (dni_cliente,id_tarjeta_embarque),
+    foreign key (dni_cliente) references Cliente (id) on delete cascade on update cascade,
+    foreign key (id_tarjeta_embarque) references Tarjeta_embarque (id) on delete cascade on update cascade
 );
